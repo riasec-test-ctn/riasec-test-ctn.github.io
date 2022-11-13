30e53c18e455c966c393750cb3740c43c18d5c35
trandatdt <trandat021197@gmail.com>
2022-11-13 23:13:42 +0700

    Deploy React app to GitHub Pages

---
+++ precache-manifest.65f898bf8597b0e387fbe73f1090752b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "84bbbd83d1ebf7189ca4",
+    "url": "/app/static/css/main.18f26c1b.chunk.css"
+  },
+  {
+    "revision": "84bbbd83d1ebf7189ca4",
+    "url": "/app/static/js/main.84bbbd83.chunk.js"
+  },
+  {
+    "revision": "5a80405e8fff7c81f25f",
+    "url": "/app/static/js/1.5a80405e.chunk.js"
+  },
+  {
+    "revision": "10256b2700850e285109",
+    "url": "/app/static/js/runtime~main.10256b27.js"
+  },
+  {
+    "revision": "84287d09b8053c6fa598893b8910786a",
+    "url": "/app/static/media/logo.84287d09.svg"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/app/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "a145aa8a67aeeb3187e704869c47492a",
+    "url": "/app/index.html"
+  }
+];
